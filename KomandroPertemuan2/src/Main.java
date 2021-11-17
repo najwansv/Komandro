82cd045332aaca950f27e2b4ccaac360b0870f11
najwansv <najwan.joan.sv@gmail.com>
2021-11-17 16:00:09 +0700

    menambahkan input

--- KomandroPertemuan2/src/Main.java
+++ KomandroPertemuan2/src/Main.java
@@ -1,8 +1,33 @@
 package KomandroPertemuan2.src;
 
+import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("halo");
+        // deklarasi var
+        String namaLengkap, namaPendek;
+        int usia;
+
+        // membuat scanner
+        Scanner keyboard = new Scanner(System.in);
+
+
+        System.out.println("halo selamat datang");
+        System.out.println("silahkan datang di rental kami");
+
+        System.out.print("Masukkan nama lengkap: ");
+        namaLengkap = keyboard.nextLine();
+        
+        System.out.print("Masukkan nama pendek: ");
+        namaPendek = keyboard.nextLine();
+        
+        System.out.print("Masukkan usia: ");
+        usia = keyboard.nextInt();
+
+        // menampilkan yang diimput
+        System.out.println("======================");
+        System.out.println("Nama lengkap: " + namaLengkap);
+        System.out.println("Nama pendek: " + namaPendek);
+        System.out.println("usia: " + usia);
     }
 }
