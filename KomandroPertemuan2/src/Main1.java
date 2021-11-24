6bb5811426dea0e26a5b2e424cc978e9cebe17a4
najwansv <najwan.joan.sv@gmail.com>
2021-11-24 13:42:01 +0700

    Arraylist

---
+++ KomandroPertemuan2/src/Main1.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Main1 {
+    
+    public static void main(String[] args) {
+        
+        ArrayList<String> namaLengkap = new ArrayList<>();
+        ArrayList<Integer> umur = new ArrayList<>();
+
+        namaLengkap.add("najwan sulvadli");
+        umur.add(12);
+
+        namaLengkap.add("John wick");
+        umur.add(29);
+
+        namaLengkap.add("tetua bangka");
+        umur.add(130);
+
+        System.out.println("Nama : " + namaLengkap.get(0));
+        System.out.println("Umur : " + umur.get(0));
+
+        System.out.println("Nama : " + namaLengkap.get(1));
+        System.out.println("Umur : " + umur.get(1));
+
+        System.out.println("Nama : " + namaLengkap.get(2));
+        System.out.println("Umur : " + umur.get(2));
+
+        System.out.println("=======================");
+
+        for( int urutan = 0 ; urutan < 3 ; urutan++ ){
+
+            
+        }
+    }
+
+}
