2876a8717c9c7b53bd12f561c030f6b397d38bbf
najwansv <najwan.joan.sv@gmail.com>
2021-11-24 14:33:26 +0700

    .

--- KomandroPertemuan2/src/Main2.java
+++ KomandroPertemuan2/src/Main2.java
@@ -3,9 +3,6 @@ import java.util.ArrayList;
 public class Main1 {
     
     public static void main(String[] args) {
-        String kelas = "Basic Programming";
-
-        intro(kelas);
         
         ArrayList<String> namaLengkap = new ArrayList<>();
         ArrayList<Integer> umur = new ArrayList<>();
@@ -18,32 +15,7 @@ public class Main1 {
 
         namaLengkap.add("tetua bangka");
         umur.add(130);
-        
-        tampilkanManual(namaLengkap, umur);
 
-        System.out.println("=======================");
-        
-        tampilkanAutomatis(namaLengkap, umur);
-
-        int panjang = 50;
-        int lebar = 30;
-
-        int hasil = hitungLuaspersegiPanjang(panjang, lebar);
-
-        System.out.println("Hasil luas = " + hasil);
-    }
-
-    public static int hitungLuaspersegiPanjang(int panjang, int lebar) {
-        int luas = panjang * lebar;
-
-        return luas;
-    }
-
-    public static void intro(String kelasKomandro) {
-        System.out.println("halo, selamat datang di kelas komandro " + kelasKomandro);
-    }
-
-    public static void tampilkanManual(ArrayList<String> namaLengkap, ArrayList<Integer> umur) {
         System.out.println("Nama : " + namaLengkap.get(0));
         System.out.println("Umur : " + umur.get(0));
 
@@ -52,15 +24,15 @@ public class Main1 {
 
         System.out.println("Nama : " + namaLengkap.get(2));
         System.out.println("Umur : " + umur.get(2));
-
-    }
-
-    public static void tampilkanAutomatis(ArrayList<String> namaLengkap, ArrayList<Integer> umur) {
+        
+        System.out.println("=======================");
+        
         for( int urutan = 0 ; urutan < 3 ; urutan++ ){
             System.out.println("perulangan ke : " + urutan);
             System.out.println("nama : " + namaLengkap.get (urutan));
             System.out.println("Umur : " + umur.get(urutan));
         }
+
         System.out.println("perulangan selesai");
     }
 
